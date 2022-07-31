c4b8d3ce517121294a83fe87533fcd295d8a6338
Iryna Shandar <iryna.shandar@gmail.com>
2022-08-01 01:08:42 +0300

    manual

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import TagsInput from "./Components/Tagsinput";
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Enter resources</h2>
+       <TagsInput/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Tagsinput.js
@@ -0,0 +1,43 @@
+// import { useState } from 'react';
+
+// function TagsInput() {
+//     const [tags, setTags] = useState([])
+
+//     function handleKeyDown(e) {
+//         if (e.key !== 'Enter')
+//             return
+//         const value = e.target.value
+//         if (!value.trim())
+//             return
+//         setTags([...tags, value])
+//         e.target.value = ''
+//         // if (tags > 3) {
+//         //     alert("Stop")
+//         // }
+//     }
+
+//     function removeTag(index) {
+//         setTags(tags.filter((e, i) => i !== index))
+//     }
+
+//     return (
+//         <div className="tags-input-container">
+//             <div>          
+//                 {tags.map((tag, index) => (
+//                     <div className="tag-item" key={index}>
+//                         <span className="text"> {tag} </span>
+//                         <span className="close" onClick={()=> removeTag(index)}>&times;</span>
+//                     </div>
+//             ))}
+//             </div>
+//             <div className="tags-input-button">
+//                 <input onKeyDown={handleKeyDown} type="text" size="60" className="tags-input" placeholder="Tag sth"></input>
+//                 {/* <button type='button' onClick={() => handleKeyDown}>srdtfybhjk</button> */}
+//             </div>
+            
+//         </div>
+// )
+// }
+
+// export default TagsInput;
+
